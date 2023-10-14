c8b3c2fef49402e638e449c3cf88d9fb3e18723f
BKG <52617079+Bijoy-910@users.noreply.github.com>
2023-10-14 20:07:38 +0530

    Add files via upload

---
+++ Project_Solutions/AciCharacter.java
@@ -0,0 +1,9 @@
+package com.Project17;
+
+public class AciCharacter {
+    public static void main(String[] args) {
+        char ch = 'z';
+        int n = ch;
+        System.out.println("The ASCii value of "+ch+" is: "+n);
+    }
+}
